08c1d1937c66749dd4f8082430f6b24e7295cb99
Amit <amittiwari8691@gmail.com>
2020-06-26 21:30:27 +0530

    added

--- src/app/components/create-project/create-project.component.ts
+++ src/app/components/create-project/create-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-project',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-project.component.scss']
 })
 export class CreateProjectComponent implements OnInit {
-
-  constructor() { }
+  projectForm: FormGroup;
+  loading = false;
+  submitted = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+    this.projectForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required]
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.projectForm.controls; }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.projectForm.invalid) {
+      this.projectForm.markAllAsTouched();
+      return;
+    }
+  }
 }
