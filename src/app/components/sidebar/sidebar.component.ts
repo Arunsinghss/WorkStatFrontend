d09edde920e308d8b96abd1db197a77aaa5ecfc1
Amit <amittiwari8691@gmail.com>
2020-06-27 17:12:50 +0530

    added

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareService } from 'src/app/services/share/share.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  userDeatils: any;
 
-  constructor() { }
+  constructor(
+    private shareService: ShareService
+  ) { }
 
   ngOnInit() {
+    this.userDeatils = this.shareService.getUserDetails();
+    console.log(this.userDeatils);
   }
 
 }
